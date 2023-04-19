a881703fb5ec354f49cf131a13c12d1c9b7aa5da
szary_ms <marcin.siara.ms@gmail.com>
2023-04-19 09:55:00 +0200

    Created Content Textarea for HTML. Created CSS Textarea to include CSS to sheet. Created Swith to optionally hide texboxt on sheet.

--- sxCssTextbox.js
+++ sxCssTextbox.js
@@ -4,13 +4,70 @@ function ( qlik) {
 
 	return {
 		support : {
-			snapshot: true,
-			export: true,
+			snapshot: false,
+			export: false,
 			exportData : false
 		},
-		paint: function ($element) {
+		definition : {
+			type : "items",
+			component : "accordion",
+			items: {
+				settings: {
+					uses: "settings",
+					items: {
+						sxOptions: {
+							label: "Content",
+							items: {
+								ContentTextarea: {
+									label:"Content HTML",
+									component: "textarea",
+									rows: 7,//the amount of rows in the textarea component (default is 3)
+									//maxlength: 100,//will not allow more than 100 characters
+									ref: "sxProp.contentTextarea",
+									expression: "optional"
+								},
+								CssTextarea: {
+									label:"Include CSS",
+									component: "textarea",
+									rows: 7,//the amount of rows in the textarea component (default is 3)
+									//maxlength: 100,//will not allow more than 100 characters
+									ref: "sxProp.cssTextarea",
+									expression: "optional"
+								},
+								UseCSSOnly: {
+									type: "boolean",
+									component: "switch",
+									label: "Use CSS Only - hide textbox",
+									ref: "sxProp.CssOnly",
+									options: [{
+										value: true,
+										label: "On"
+									}, {
+										value: false,
+										label: "Not On"
+									}],
+									defaultValue: false
+								}
+							}
+						}
+
+					}
+				}
+			}
+		},
+		paint: function ($element, layout) {
 			//add your rendering code here
-			$element.html( "sxCssTextbox" );
+			var html = '<style>'+layout.sxProp.cssTextarea+'</style>';
+			
+			if(layout.sxProp.CssOnly == true) {
+				html += '<style>[tid="'+layout.qInfo.qId+'"] article:not(.qv-mode-edit) { visibility: hidden; }</style>';
+			}
+			
+			if(layout.sxProp.CssOnly != true) {
+				html += '<div class="sxCssTextbox_container">'+layout.sxProp.contentTextarea+'</div>';
+			}
+			
+			$element.html(html);
 			//needed for export
 			return qlik.Promise.resolve();
 		}
